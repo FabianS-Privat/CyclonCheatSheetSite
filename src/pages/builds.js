160d47c1d9588479d6f49ee48d2fb172f6d81da6
Fabian Szönyi <f.szoenyi@gmail.com>
2025-09-25 10:25:59 +0200

    ad fix attempt 2

--- src/pages/builds.js
+++ src/pages/builds.js
@@ -5,19 +5,21 @@ export default function About() {
   return (
     <Layout>
       <div class="h-full w-full">
-        <script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4659198126450802"
-          crossorigin="anonymous"
-        ></script>
-        <ins
-          class="adsbygoogle block"
-          data-ad-client="ca-pub-4659198126450802"
-          data-ad-slot="2573850842"
-          data-ad-format="auto"
-          data-full-width-responsive="true"
-        ></ins>
-        <script>(adsbygoogle = window.adsbygoogle || []).push({});</script>
+        <div id="ad-container block" class="my-4 flex justify-center">
+          <script
+            async
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4659198126450802"
+            crossorigin="anonymous"
+          ></script>
+          <ins
+            class="adsbygoogle"
+            data-ad-client="ca-pub-4659198126450802"
+            data-ad-slot="2573850842"
+            data-ad-format="auto"
+            data-full-width-responsive="true"
+          ></ins>
+          <script>(adsbygoogle = window.adsbygoogle || []).push({});</script>
+        </div>
         <iframe
           class="h-full w-full"
           src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQPWKhEapD7L4r7WYacTwz-xKK36ZKS9VncxJ7yg0ZemuwF2uJN-RjR-3pLe6j6kJGgdmzhRItp47uF/pubhtml?widget=true&amp;headers=false"
