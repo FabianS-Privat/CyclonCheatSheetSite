bdc12e5ce372ec2d9c6ed7482d071a453d23d595
Fabian Szönyi <f.szoenyi@gmail.com>
2026-03-22 09:45:41 +0100

    Fix Landing Page

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -11,17 +11,19 @@ export default function HomepageFeatures() {
             <div className="text--center padding-horiz--md">
               <Heading as="h3">Notice</Heading>
               <p>
-                Full Transparency, this Site got way more visits than expected
-                and as a result is more expensive too. <br />I would highly
-                appreciate it, if you could disable your Ad-Blocker. That way I
-                could cover part of the Cost for this Website with Ad-Revenue.
+                Hello all, I apologize for the long Downtime of the Site during
+                3.28 Leauge Launch, the Site got a massive Spike in Visits and
+                blew the Budget I had hosting with Vercel. I had to cancel the
+                plan and find a alternative Solution. It is now hosted via
+                GitHub Pages, which should be free, but you might encounter
+                longer load times. I apoligize again for any inconvenience
+                caused.
               </p>
               <p>
-                Alternativly, if you would like to support me directly, you can
-                do so via Patreon or with a One-Time Donation. As little as 1 $
-                per month would help me a lot to cover the Cost. <br />( I
-                addeda a 1€ Tier, but Patreon hides it on the initial view, its
-                left of the Splinter Supporter.)
+                If you would like to support me, you can do so via Patreon or
+                with a One-Time Donation. Even just 1 $ per month would mean a
+                lot to me. <br />( I addeda a 1€ Tier, but Patreon hides it on
+                the initial view, its left of the Splinter Supporter.)
               </p>
               <Heading as="h3">Patreon</Heading>
               <div class="h-[90px] w-fit bg-grey-800 m-auto flex flex-col">
@@ -34,10 +36,9 @@ export default function HomepageFeatures() {
               </div>
 
               <p> Thanks to everyone supporting my content:</p>
-              <h1>Monolith Supporter</h1>
-              <p> TempestMind, Kesuke</p>
               <h2>Emblem Supporter</h2>
-              <p> Morfyzm, (des)mosthenes</p>
+              <p> Kesuke, Morfyzm, (des)mosthenes, deedai</p>
+              <p> </p>
             </div>
           </div>
         </div>
